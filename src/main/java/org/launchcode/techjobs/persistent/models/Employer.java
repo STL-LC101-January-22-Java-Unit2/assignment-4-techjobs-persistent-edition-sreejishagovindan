6dac2ee8ef91c02b37908d4a080d3a0a60012a46
sreejishagovindan <100387980+sreejishagovindan@users.noreply.github.com>
2022-07-06 20:38:42 -0500

    Completed code

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,38 @@
 package org.launchcode.techjobs.persistent.models;
 
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    public List<Job> getJobs() {
+        return jobs;
+    }
 
+    public Employer() {
+
+    }
+
+    @NotNull
+    @Size(min = 3, max = 255)
+    private String location;
+
+    @OneToMany
+    @JoinColumn(name="employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
